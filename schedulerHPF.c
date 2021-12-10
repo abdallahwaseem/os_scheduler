e4dc9262963a10285dd3fdeb7a5ee76b9252c6eb
Mustafa Usama <mustafausamaa@gmail.com>
2021-12-11 01:56:26 +0200

    Update schedulerHPF.c

--- schedulerHPF.c
+++ schedulerHPF.c
@@ -1,23 +1,111 @@
 #include "headers.h"
 void handler(int signum);
 
+int runningProccessRuntime;
+
+void sigalrm_handler(int signum)
+{
+    printf("\ninside handler\n");
+    runningProccessRuntime = 0;
+}
+
 int main(int argc, char *argv[])
 {
     initClk();
     signal(SIGINT, handler);
-    // TODO implement the scheduler High Priority First:)
+
+    signal(SIGALRM, sigalrm_handler);
+    // Initialize message queue
+    key_t key_id = ftok("file_hayet3emel", 66);
+    int msgqid = msgget(key_id, 0644);
+    struct msgBuff message;
+
+    // For the first process received
+    int rec_val;
+    rec_val = msgrcv(msgqid, &message, sizeof(message.mtext), 0, !IPC_NOWAIT);
+    NodePQ *pq = newNodePQ(message.Data, message.Data.priority);
+    runningProccessRuntime = pq->data.remainingtime;
+
+    char runProcesstimeChar[10];
+    sprintf(runProcesstimeChar, "%d", runningProccessRuntime);
+    char *argve[] = {"process.out", runProcesstimeChar, NULL};
+    int pid = fork();
+    if (pid == 0)
+    {
+        execve(argve[0], &argve[0], NULL);
+    }
+    alarm(runningProccessRuntime);
+    pause();
+
+    // For the rest of the processes
+    while (1)
+    {
+        rec_val = msgrcv(msgqid, &message, sizeof(message.mtext), 0, !IPC_NOWAIT);
+        if (rec_val == -1)
+        {
+            perror("Error in receive");
+        }
+        pushPQ(pq, message.Data, message.Data.priority);
+        if (message.count != 0)
+        {
+            for (int i = 0; i < message.count; i++)
+            {
+                rec_val = msgrcv(msgqid, &message, sizeof(message.mtext), 0, !IPC_NOWAIT);
+                pushPQ(pq, message.Data, message.Data.priority);
+            }
+        }
+        // TODO Get from the process generator
+
+        if (runningProccessRuntime == 0)
+        {
+            processCreator();
+            // runningProccessRuntime = pq->data.remainingtime;
+            // argve[] = {"process.out", runProcesstimeChar, NULL};
+            // sprintf(runProcesstimeChar, "%d", runningProccessRuntime);
+            // char *argve[] = {"process.out", runProcesstimeChar, NULL};
+            // int pid = fork();
+            // if (pid == 0)
+            // {
+            //     execve(argve[0], &argve[0], NULL);
+            // }
+            // popPQ(pq);
+            // alarm(runningProccessRuntime);
+            // pause();
+        }
+        else
+        {
+            // dequeue from ready queue
+
+            // add in shared memory
+
+            // run process
+        }
+    }
+
     printf("\nThis is HPF\n");
     // upon termination release the clock resources
-    //destroyClk(true);
-    while(1)
-    {
-
-    }
-    
+    printf("\nAbout to destroy the shared memory area !\n");
+    destroyClk(true);
 }
 
 void handler(int signum)
 {
     destroyClk(true);
     exit(getpid());
+}
+
+void processCreator()
+{
+    runningProccessRuntime = pq->data.remainingtime;
+    argve[] = {"process.out", runProcesstimeChar, NULL};
+    sprintf(runProcesstimeChar, "%d", runningProccessRuntime);
+    char *argve[] = {"process.out", runProcesstimeChar, NULL};
+    int pid = fork();
+    if (pid == 0)
+    {
+        execve(argve[0], &argve[0], NULL);
+    }
+    popPQ(pq);
+    alarm(runningProccessRuntime);
+    pause();
 }
